e79ff2d2b3dd693834c78eb38b8ebc18e1985ed9
Gautier Boder <gboder@outlook.com>
2024-02-08 16:14:30 +0000

    Remove unused Azure CLI persistence feature and simplify infrastructure deployment

--- deploy/deploy.infra.sh
+++ deploy/deploy.infra.sh
@@ -20,30 +20,17 @@ az deployment group validate --resource-group $resourcegroup --template-file iac
     serviceConnectionPrincipal='' \
     applicationName='prvfct' \
     uniqueString=$unique \
-    keyVaultSku='standard' \
-    storageAccountSkuName='Standard_LRS' \
-    vnetName=$vnet \
-    endpointSubnetName=$subnetpep \
-    kvPrivateDnsZoneId=$kvPrivateDnsZoneId \
-    devEntraIdGroupIdForKvAccessPolicies=$resourceAdminId \
-    storageBlobPrivateDnsZoneId=$storageBlobPrivateDnsZoneId \
-    storageFilePrivateDnsZoneId=$storageFilePrivateDnsZoneId
+    devEntraIdGroupIdForKvAccessPolicies=$resourceAdminId
 
 # deploy infarstructure
-az deployment group create --resource-group $resourcegroup --template-file iac/infrastructure.bicep --name $deploymentName \
+az deployment group create --resource-group $resourcegroup --template-file iac/infrastructure.bicep \ 
+    --name $deploymentName \
     --parameters location=$location \
     environment='dev' \
     serviceConnectionPrincipal='' \
     applicationName='prvfct' \
     uniqueString=$unique \
-    keyVaultSku='standard' \
-    storageAccountSkuName='Standard_LRS' \
-    vnetName=$vnet \
-    endpointSubnetName=$subnetpep \
-    kvPrivateDnsZoneId=$kvPrivateDnsZoneId \
-    devEntraIdGroupIdForKvAccessPolicies=$resourceAdminId \
-    storageBlobPrivateDnsZoneId=$storageBlobPrivateDnsZoneId \
-    storageFilePrivateDnsZoneId=$storageFilePrivateDnsZoneId
+    devEntraIdGroupIdForKvAccessPolicies=$resourceAdminId
 
 # retrieve the output of the deployment
 echo "Retrieve the output of the deployment"
