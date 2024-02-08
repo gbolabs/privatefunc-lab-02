68c976132f4d72bc56fed3ecff1af5241d1f2381
Gautier Boder <gboder@outlook.com>
2024-02-08 16:13:55 +0000

    Remove debug flag from bicep file validation

--- deploy/deploy.infra.sh
+++ deploy/deploy.infra.sh
@@ -14,7 +14,7 @@ storageFilePrivateDnsZoneId=$(az network private-dns zone show --name "privateli
 
 # validate the bicep file
 echo "Validate the bicep file"
-az deployment group validate --debug --resource-group $resourcegroup --template-file iac/infrastructure.bicep \
+az deployment group validate --resource-group $resourcegroup --template-file iac/infrastructure.bicep \
     --parameters location=$location \
     environment='dev' \
     serviceConnectionPrincipal='' \
