72c0f5475049031fe717b1de17481637fa6755aa
Gautier Boder <gboder@outlook.com>
2024-02-08 10:01:28 +0000

    Update network and subnet names in variables.sh

--- deploy/env/variables.sh
+++ deploy/env/variables.sh
@@ -9,11 +9,11 @@ resourceAdminId="74fa1dc1-96ae-4b65-9905-9004b475ff9d"
 # copy of the generated name value used when recovering the key vault
 keyVaultName="gbl-prvfct-kv-dev-$unique"
 
-vnet="gbl-mwbh-vnet-dev-$unique"
+vnet="gbl-prvfct-vnet-dev-$unique"
 vnetAddress="192.168.50.0/24"
-subnetpep="subnet-pep"
+subnetpep="gbl-prvfct-snet-dev-001"
 pepAddress="192.168.50.0/27"
-subnetapp="subnet-app"
+subnetapp="gbl-prvfct-snet-dev-002"
 appAddress="192.168.50.32/27"
 
 vnetVm="gbl-devvm-vnet-dev-$unique"
