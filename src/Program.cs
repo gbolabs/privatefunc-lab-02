c68279271a20f2c1964518901a831a9f5482198b
Gautier Boder <gboder@outlook.com>
2024-02-12 09:04:35 +0100

    Add user secrets configuration

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration(configureDelegate=> {
+        // Add configuration sources
+        configureDelegate.AddUserSecrets<Program>();
+    })
     .Build();
 
 host.Run();
